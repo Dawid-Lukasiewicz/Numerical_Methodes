f44ad619abea7cfc2d6a3b9c1f5ab1b0e6bbb172
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-03-04 01:29:09 +0100

    random float values for augmented matrix

--- Lab1/zad1.py
+++ Lab1/zad1.py
@@ -1,6 +1,7 @@
 import argparse as argp
 import numpy as np
 import scipy
+from random import uniform
 
 def print_augm_matrix(inputMatrix, size):
     for r in range(size):
@@ -9,6 +10,11 @@ def print_augm_matrix(inputMatrix, size):
             print(" ", inputMatrix[r][c], " ", end="")
         print("| ", inputMatrix[r][size], " ]")
 
+def random_aug_matrix(inputMatrix, size):
+    for r in range(size):
+        for c in range(size):
+            inputMatrix[r][c] = uniform(0, 10)
+        inputMatrix[r][size] = uniform(0, 10)
 
 def gauss_elimination(inputMatrix, size):
     print("Gauss not implemented yet")
@@ -20,4 +26,6 @@ Ab = np.zeros(shape=(N, N+1))
 # Solution vector of N elements
 x = np.zeros(shape=N)
 
+
+random_aug_matrix(Ab, N)
 print_augm_matrix(Ab, N)
