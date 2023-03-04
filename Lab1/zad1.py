627b585e14d9c0a0baf54dd5eda38247f22b28be
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-03-04 01:03:11 +0100

    printing the augmented matrix

--- Lab1/zad1.py
+++ Lab1/zad1.py
@@ -1,8 +1,23 @@
-import argparse
-import numpy
+import argparse as argp
+import numpy as np
 import scipy
 
-def gauss_elimination():
-    print("Gauss")
+def print_augm_matrix(inputMatrix, size):
+    for r in range(size):
+        print("[ ", end="")
+        for c in range(size):
+            print(" ", inputMatrix[r][c], " ", end="")
+        print("| ", inputMatrix[r][size], " ]")
 
-gauss_elimination()
+
+def gauss_elimination(inputMatrix, size):
+    print("Gauss not implemented yet")
+
+N = 3
+
+# Matrix input NxN+1
+Ab = np.zeros(shape=(N, N+1))
+# Solution vector of N elements
+x = np.zeros(shape=N)
+
+print_augm_matrix(Ab, N)
