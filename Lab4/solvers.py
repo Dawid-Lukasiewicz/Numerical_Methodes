46b6f779a5e0d12273743d15ab991c48b5c73d8f
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-04-16 21:03:18 +0200

    fixing break condition and identity matrix muliply fix

--- Lab4/solvers.py
+++ Lab4/solvers.py
@@ -6,11 +6,11 @@ import numpy as np
 import scipy as sci
 import matrix_handler as mx
 
-def focuss_algorithm(A, x, b, p=1, h=1):
-    e = pow(10, -5)
+def focuss_algorithm(A, x, b, p=1, h=1, epsilon=pow(10, -5)):
     M = len(x)
     W = np.zeros([M, M], float)
-    for _ in range(10**5):
+    normL2 = pow(np.linalg.norm( A @ x - b ), 2) + np.sum(pow(abs(x), p))
+    while True:
         # x^(1-(p/2))
         x = np.float_power(np.fabs(x), 1-(p/2))
         # Then input the x elements to diagonal W
@@ -18,12 +18,11 @@ def focuss_algorithm(A, x, b, p=1, h=1):
 
         #x_{k+1} =  W^2(x_k) * A^T (A * W^2(x_k) * A^T + h * Im)^(-1) * b
         part1 = np.linalg.matrix_power(W, 2) @ A.T
-        part2 = np.linalg.inv( A @ np.linalg.matrix_power(W, 2) @ A.T + np.multiply(h, np.ones([M, M], float)) )
+        part2 = np.linalg.inv( A @ np.linalg.matrix_power(W, 2) @ A.T + np.multiply(h, np.diag(np.ones(M))) )
         x = part1 @ part2 @ b
         # ||Ax - b||^2 + E^p(x) < epsilon
-        Ep = np.sum(pow(abs(x), p))
-        l2 = pow(np.linalg.norm( A @ x - b ), 2) + Ep
-        if l2 < e:
+        normL2Old = normL2
+        normL2 = pow(np.linalg.norm( A @ x - b ), 2) + np.sum(pow(abs(x), p))
+        if np.fabs(normL2 - normL2Old) < epsilon:
             break
-
     return x
