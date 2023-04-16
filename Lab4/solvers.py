1223a1c8c121aa7a021ff6224801e95095c99827
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-04-16 20:06:52 +0200

    focuss algorithm step fix

--- Lab4/solvers.py
+++ Lab4/solvers.py
@@ -6,21 +6,24 @@ import numpy as np
 import scipy as sci
 import matrix_handler as mx
 
-def focuss_algorithm(A, x, b, p):
+def focuss_algorithm(A, x, b, p=1, h=1):
     e = pow(10, -5)
+    M = len(x)
+    W = np.zeros([M, M], float)
     for _ in range(10**5):
         # x^(1-(p/2))
         x = np.float_power(np.fabs(x), 1-(p/2))
-
         # Then input the x elements to diagonal W
-        W = np.zeros([len(x), len(x)], float)
         np.fill_diagonal(W, x)
 
+        #x_{k+1} =  W^2(x_k) * A^T (A * W^2(x_k) * A^T + h * Im)^(-1) * b
+        part1 = np.linalg.matrix_power(W, 2) @ A.T
+        part2 = np.linalg.inv( A @ np.linalg.matrix_power(W, 2) @ A.T + np.multiply(h, np.ones([M, M], float)) )
+        x = part1 @ part2 @ b
         # ||Ax - b||^2 + E^p(x) < epsilon
-        x = np.diag(W)
         Ep = np.sum(pow(abs(x), p))
         l2 = pow(np.linalg.norm( A @ x - b ), 2) + Ep
         if l2 < e:
             break
-        
+
     return x
