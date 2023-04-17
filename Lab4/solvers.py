8e0f78ac221f2f40efa8b74e4eacc555e69affec
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-04-18 00:05:46 +0200

    fragment FOCUSS algorithm

--- Lab4/solvers.py
+++ Lab4/solvers.py
@@ -6,6 +6,13 @@ import numpy as np
 import scipy as sci
 import matrix_handler as mx
 
+#x_{k+1} =  W^2(x_k) * A^T (A * W^2(x_k) * A^T + h * Im)^(-1) * b
+def focuss_deriverative_multiplication_step(A, W, b, h=1):
+    M, _ = A.shape
+    part1 = np.linalg.matrix_power(W, 2) @ A.T # W^2(x_k) * A^T
+    part2 = A @ np.linalg.matrix_power(W, 2) @ A.T # A * W^2(x_k) * A^T
+    part3 = np.multiply(h, np.diag(np.ones(M))) # h * Im
+    return part1 @ np.linalg.inv( part2 + part3 ) @ b
 
 def focuss_algorithm(A, b, x=None, p=1, h=1, epsilon=pow(10, -5)):
     M, N = A.shape
@@ -20,11 +27,7 @@ def focuss_algorithm(A, b, x=None, p=1, h=1, epsilon=pow(10, -5)):
         # Then input the x elements to diagonal W
         np.fill_diagonal(W, x)
 
-        #x_{k+1} =  W^2(x_k) * A^T (A * W^2(x_k) * A^T + h * Im)^(-1) * b
-        part1 = np.linalg.matrix_power(W, 2) @ A.T # W^2(x_k) * A^T
-        part2 = A @ np.linalg.matrix_power(W, 2) @ A.T # A * W^2(x_k) * A^T
-        part3 = np.multiply(h, np.diag(np.ones(M))) # h * Im
-        x = part1 @ np.linalg.inv( part2 + part3 ) @ b
+        x = focuss_deriverative_multiplication_step(A, W, b, h)
         
         # ||Ax - b||^2 + E^p(x) < epsilon
         normL2Old = normL2
