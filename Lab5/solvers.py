1a4f35bb1c2a0ce5380baa0173a578900dfccebd
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-04-22 02:52:37 +0200

    adjust my matrix calls and numpy imports

--- Lab5/solvers.py
+++ Lab5/solvers.py
@@ -4,10 +4,7 @@ from IPython.display import display, Math
 import os
 
 import numpy as np
-from numpy import matmul
-from numpy import dot
 from numpy import fabs
-from numpy import multiply
 from numpy.linalg import norm
 from numpy.linalg import eigvals
 
@@ -65,7 +62,7 @@ def landweber(A, b, x=None, alpha=0.5, maxIter=100, epsilon=1e-5):
         # x(k+1) = x(k) + alpha * A^T * (b - A * x)
         # but convergence never occurs if A^T
         """ x(k+1) = x(k) + alpha * (b - A * x) """
-        x = x + multiply(alpha, (b - matmul(A, x)))
+        x = x + alpha * (b - A @ x)
 
         normL2Old = normL2
         normL2 = norm(x)
