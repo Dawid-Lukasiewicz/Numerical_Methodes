f2d8af57af3ee80b49b256179123cc4546f4bd77
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-06-10 18:56:04 +0200

    there is something wrong about break condition

--- solvers_lab8.py
+++ solvers_lab8.py
@@ -8,25 +8,27 @@ def Newton_Gauss(f, jac, x0, tol=1e-6, max_iter=100):
         F = -f(x)
         J = jac(x)
         p = np.dot(inv(J), F)
+        # p = np.linalg.solve(J, F)  # Solving the linear system J * delta_x = -F
         
         x += p
         
         if np.linalg.norm(p) < tol:
             break
     
-    return x
+    return x, i
 
-def Damped_Newton_Gauss(f, jac, x0, damping_factor=1.0, tol=1e-6, max_iter=100):
+def Damped_Newton_Gauss(f, jac, x0, damping_factor=0.01, tol=1e-6, max_iter=100):
     x = x0
     
     for i in range(max_iter):
         F = f(x)
         J = jac(x)
         p = np.dot(inv(J + damping_factor*np.eye(len(x))), -F)
+        # p = np.linalg.solve(J + damping_factor*np.eye(len(x)), -F) 
         
         x += p
         
         if np.linalg.norm(p) < tol:
             break
     
-    return x
+    return x, i
